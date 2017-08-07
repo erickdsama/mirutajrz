3fd282cdbcf6882da91cc84d812b2b925e022bcd
erickdsama <Ramses22>
2017-08-07 15:06:09 -0600

    lo estoy intentando :|

--- inicio/views.py
+++ inicio/views.py
@@ -51,17 +51,16 @@ class InicioView(View):
             # print "salgo", coordendada.distance, coordendada
             ruta = coordendada.ruta
 
-        return render(request, self.template_name, context={"ruta":ruta})
+        return render(request, self.template_name, context={"ruta": ruta})
 
 
 class ProcesaArchivo(View, LoginRequiredMixin):
-
     def get(self, request):
-        #obtener todas las rutas
+        # obtener todas las rutas
         rutas = Ruta.objects.all()
         RutaCoordenda.objects.all().delete()
         for ruta in rutas:
-            kml_file =  os.path.join(BASE_DIR+"/media/", str(ruta.kml))
+            kml_file = os.path.join(BASE_DIR + "/media/", str(ruta.kml))
             print kml_file
             kml = ProcessKMLFile(kml_file)
             route_obj = kml.file_to_objet()
@@ -74,25 +73,23 @@ class ProcesaArchivo(View, LoginRequiredMixin):
                 z = split_coord[2]
                 point = GEOSGeometry("POINT({0} {1})".format(x, y))
                 RutaCoordenda.objects.create(ruta=ruta, coordenadas=point)
-            # soriana henequen 31.6482147,-106.3911803
-            # smart libramiento 31.627863,-106.354794
+                # soriana henequen 31.6482147,-106.3911803
+                # smart libramiento 31.627863,-106.354794
 
-
-        return JsonResponse(data=[],safe=False)
+        return JsonResponse(data=[], safe=False)
 
 
 class GetRuta(APIView):
-
     def post(self, request):
         data_post = request.data
-        lat_in =  data_post.get("lat_in") #y
-        lon_in =  data_post.get("lon_in") #x
-        lat_go =  data_post.get("lat_go") #y
-        lon_go =  data_post.get("lon_go") #x
+        lat_in = data_post.get("lat_in")  # y
+        lon_in = data_post.get("lon_in")  # x
+        lat_go = data_post.get("lat_go")  # y
+        lon_go = data_post.get("lon_go")  # x
         print "POINT({} {})".format(lon_in, lat_in)
         point_of_user = GEOSGeometry("POINT({} {})".format(lon_in, lat_in))
         point_to_go = GEOSGeometry("POINT({} {})".format(lon_go, lat_go))
-        max_distance = 500  # m
+        max_distance = 200  # m
 
         coordendadas = RutaCoordenda.objects.filter(
             coordenadas__distance_lt=(
@@ -121,7 +118,96 @@ class GetRuta(APIView):
             obj["id"] = coordendada.ruta.id
             obj["nombre"] = coordendada.ruta.nombre
             obj["distance"] = str(coordendada.distance)
+            obj["url"] = str(coordendada.ruta.http_kml)
             obj["kml"] = str(coordendada.ruta.kml)
             rutas_to_send.append(obj)
 
-        return Response(data=rutas_to_send, status=200)
+        if len(rutas_to_send) > 0:
+            return Response(data=rutas_to_send, status=200)
+        else:
+            # try to get transboarding route
+            rutas_to_send =  self.transborde(point_of_user, point_to_go)
+            return Response(data=rutas_to_send, status=200)
+
+    def transborde(self, punto_partida, punto_final):
+        print "intentanto fijar ruta de transborde"
+
+        # obtener rutas en punto de partida
+        # punto_partida = GEOSGeometry("POINT({} {})".format(lon_in, lat_in))
+        max_distance = 200  # m
+
+        rutas_iniciales = RutaCoordenda.objects.filter(
+            coordenadas__distance_lt=(
+                punto_partida,
+                Distance(m=max_distance)
+            ),
+        ).annotate(distance=qDistance('coordenadas', punto_partida)).distinct("ruta")
+
+        # print rutas_iniciales
+
+        # obtener rutas del punto deseado
+        # punto_final = GEOSGeometry("POINT({} {})".format(lon_go, lat_go))
+        max_distance = 200  # m
+
+        rutas_finales = RutaCoordenda.objects.filter(
+            coordenadas__distance_lt=(
+                punto_final,
+                Distance(m=max_distance)
+            ),
+        ).annotate(distance=qDistance('coordenadas', punto_final)).distinct("ruta")
+
+        print rutas_iniciales
+        print "*"*20
+        print rutas_finales
+        # script para buscar coordenadas coicidan
+
+        #   obtener coordenadas de
+        rutas = []
+        for ruta_final in rutas_finales:
+            print "obtiene las rutas de donde vamos"
+            coordendas_ruta = RutaCoordenda.objects.filter(ruta=ruta_final.ruta)
+            for coordenada in coordendas_ruta:
+                for ruta_inicial in rutas_iniciales:
+                    # print "buscando en las rutas iniciales", coordenada.coordenadas, ruta_inicial, ruta_final
+                    buscador = RutaCoordenda.objects.filter(
+                        coordenadas__distance_lt=(
+                            coordenada.coordenadas,
+                            Distance(m=50)
+                        ),
+                        ruta=ruta_inicial.ruta
+                    ).annotate(distance=qDistance('coordenadas', punto_final)).distinct("ruta")
+                    if len(buscador) > 0:
+                        obj = {}
+                        obj["id"] = ruta_final.ruta.id
+                        obj["nombre"] = ruta_final.ruta.nombre
+                        obj["distance"] = str(ruta_final.distance)
+                        obj["url"] = str(ruta_final.ruta.http_kml)
+                        obj["kml"] = str(ruta_final.ruta.kml)
+                        obj2 = {}
+                        obj2["id"] = ruta_inicial.ruta.id
+                        obj2["nombre"] = ruta_inicial.ruta.nombre
+                        obj2["distance"] = str(ruta_inicial.distance)
+                        obj2["url"] = str(ruta_inicial.ruta.http_kml)
+                        obj2["kml"] = str(ruta_inicial.ruta.kml)
+
+                        if not self.checas_rutas(obj["id"], rutas):
+                            rutas.append(obj)
+                        if not self.checas_rutas(obj2["id"], rutas):
+                            rutas.append(obj2)
+
+                        # for ruta in rutas:
+                        #     if
+                        #     rutas.append(obj)
+                        #
+                        # if not any(ruta.id == obj2["id"] for ruta in rutas):
+                        #     rutas.append(obj2)
+
+        return rutas
+
+
+    def checas_rutas(self, id, rutas):
+        for ruta in rutas:
+            if ruta.get("id") == id:
+                return True
+        return False
+

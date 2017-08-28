0e248c4f5a0637516564802e1b83c05dd4c2e506
erickdsama <Ramses22>
2017-08-28 14:45:21 -0600

    steper routes init

--- inicio/views.py
+++ inicio/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 import os
+import requests
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.gis.db.models.functions import Distance as qDistance
@@ -111,11 +112,11 @@ class GetRuta(APIView):
         point_of_user = GEOSGeometry("POINT({} {})".format(lon_in, lat_in))
         point_to_go = GEOSGeometry("POINT({} {})".format(lon_go, lat_go))
 
-        #revisa si se puede llegar en una sola ruta
+        # revisa si se puede llegar en una sola ruta
         rutas_salida = Ruta.objects.filter(puntos__distance_lte=(point_of_user, Distance(m=self.max_distance)))
         rutas_salida = rutas_salida.filter(puntos__distance_lte=(point_to_go, Distance(m=self.max_distance)))
 
-        #en caso de no poder intenta encontrar una ruta que transborde
+        # en caso de no poder intenta encontrar una ruta que transborde
         if len(rutas_salida) <= 0:
             rutas_salida = self.transborde(point_of_user, point_to_go)
             return Response(data=rutas_salida, status=200)
@@ -125,7 +126,7 @@ class GetRuta(APIView):
         for ruta in rutas_salida:
             obj = {}
             obj["id"] = ruta.pk
-            obj["nombre"] =ruta.nombre
+            obj["nombre"] = ruta.nombre
             obj["url"] = str(ruta.http_kml)
             obj["kml"] = str(ruta.kml)
             rutas_to_send.append(obj)
@@ -136,8 +137,6 @@ class GetRuta(APIView):
             rutas_to_send = self.transborde(point_of_user, point_to_go)
             return Response(data=rutas_to_send, status=200)
 
-
-
     def transborde(self, punto_partida, punto_final):
         rutas_salida = Ruta.objects.filter(puntos__distance_lte=(punto_partida, Distance(m=self.max_distance)))
         ruta_llegada = Ruta.objects.filter(puntos__distance_lte=(punto_final, Distance(m=self.max_distance)))
@@ -145,16 +144,18 @@ class GetRuta(APIView):
         opciones = []
         for ruta in rutas_salida:
             print ruta
-            crosses =  Ruta.objects.filter(puntos__intersects=ruta.puntos, pk__in=llegadas_ids)
+            crosses = Ruta.objects.filter(puntos__intersects=ruta.puntos, pk__in=llegadas_ids)
             ruta_obj = {}
             ruta_obj["nombre"] = ruta.nombre
             ruta_obj["url"] = ruta.http_kml
             ruta_obj["kml"] = str(ruta.kml)
+            ruta_obj["id"] = str(ruta.pk)
             opcion = []
 
             for cross in crosses:
                 cross_obj = {}
                 cross_obj["nombre"] = cross.nombre
+                cross_obj["id"] = cross.pk
                 cross_obj["url"] = cross.http_kml
                 cross_obj["kml"] = str(cross.kml)
 
@@ -170,3 +171,37 @@ class GetRuta(APIView):
             if ruta.get("id") == id:
                 return True
         return False
+
+
+class SteperByRoutes(APIView):
+    def post(self, request):
+        data_post = request.data
+        lat_go = data_post.get("lat_go", "")
+        lon_go = data_post.get("lon_go", "")
+        lat_in = data_post.get("lon_go", "")
+        lon_in = data_post.get("lon_go", "")
+        ruta_a = data_post.get("ruta_a", None)
+        ruta_b = data_post.get("ruta_b", None)
+
+        url = "https://maps.googleapis.com/maps/api/geocode/json?latlng={},{}&key=AIzaSyB2aJkKwaakfAgYg7mx_eol3-4iPFYdWXw"
+
+        # section to get places (a - b)
+        # point where i finish
+        url_go = url.format(lat_go, lon_go)
+        request_go = requests.request("get", url_go)
+        json_go = request_go.json()
+        place_go = json_go.get("results")[0].get("formatted_address")
+
+        # point where i start
+        url_in = url.format(lat_in, lon_in)
+        request_in = requests.request("get", url_in)
+        json_in = request_in.json()
+        place_in = json_in.get("results")[0].get("formatted_address")
+
+        # section to cross routes
+
+
+
+
+
+        return JsonResponse(request_go.json(), safe=False)

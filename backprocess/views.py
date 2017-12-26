c58321b88505d32b649dd12a1b61f34b0967cd2a
erickdsama <Ramses22>
2017-12-26 13:55:27 -0700

    sync response

--- backprocess/views.py
+++ backprocess/views.py
@@ -5,6 +5,7 @@ from django.http.response import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
@@ -19,4 +20,4 @@ class SyncTrack(APIView):
 
     def post(self, request):
 
-        return JsonResponse({}, safe=False)
+        return Response(data={}, status=200)
